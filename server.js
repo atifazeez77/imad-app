2568f623f17dc386eccbce07b1afdfef282599f5
atifazeez77 <30769805+atifazeez77@users.noreply.github.com>
2017-09-13 20:06:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/ui/modi.png', function (req, res) {
 });
 
 
-var port = 8080; //Use 8080 for local development because ypu might already have apache running on 80
+var port = 80; //Use 8080 for local development because ypu might already have apache running on 80
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
