3e860cf19032f98d3e25267f0c914356df60565b
atifazeez77 <30769805+atifazeez77@users.noreply.github.com>
2017-09-21 13:07:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ var button = document.getElementById('counter');
              if (request.status === 200) {
                //Capture a list of names and render it as a list
                var names = request.responseText;
-               names = JSON. parse('names');
+               names = JSON.parse('names');
                var list = '';
                for (var i=0; i<names.length; i++) {
                    list += '<li>' + names[i] + '</li>';
