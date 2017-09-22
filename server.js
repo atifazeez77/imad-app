85d00f0787f2d4902393f2a4144217b60d79feb1
atifazeez77 <30769805+atifazeez77@users.noreply.github.com>
2017-09-22 18:47:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ var articles = {
         heading: 'Article One',
         date: 'Sep 4, 2017',
         content:
-        <p>
+       <p>
          This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article
-        </p>
+            </p>
             <p>
                 This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article
             </p>
-            <p>
-                This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article.
+                      <p>
+                This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article This is the content for my first article
             </p>
 },
     'article-two'; {
